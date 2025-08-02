2292583cb697dcaa096740c346216fa01f31a38c
Malysheva Ksenia <maly6eva.ksenia@gmail.com>
2025-08-02 08:22:52 +0300

    toggle added

--- js.js
+++ js.js
@@ -1,3 +1,25 @@
 "use strict";
 
-const cart = docoment.querySelectorAll()
+const carts = document.querySelectorAll('.carts__cart')
+const button_right = document.querySelector('.button_right')
+const button_left = document.querySelector('.button_left')
+
+let currentIndex = 0
+
+carts[currentIndex].classList.add('active')
+
+button_right.addEventListener('click', () => {
+if(currentIndex < carts.length - 1){
+    carts[currentIndex].classList.remove('active')
+    currentIndex++
+    carts[currentIndex].classList.add('active')
+}
+})
+
+button_left.addEventListener('click', () => {
+    if(currentIndex > 0) {
+        carts[currentIndex].classList.remove('active')
+        currentIndex--;
+        carts[currentIndex].classList.add('active')
+    }
+})
